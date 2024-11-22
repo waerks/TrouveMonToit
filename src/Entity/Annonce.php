5c2d743397a1b98e3dede9fd1baddfdf9b31e4bf
Anaïs <a.aerts@interface3.be>
2024-11-22 13:09:56 +0100

    Add Entity and initialisation files

---
+++ src/Entity/Annonce.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnnonceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnnonceRepository::class)]
+class Annonce
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 12, scale: 2)]
+    private ?string $prix = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_publication = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $etat = null;
+
+    #[ORM\Column]
+    private ?int $nombre_etages = null;
+
+    #[ORM\Column]
+    private ?int $nombre_facades = null;
+
+    #[ORM\Column]
+    private ?int $parking = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTimeInterface
+    {
+        return $this->date_publication;
+    }
+
+    public function setDatePublication(\DateTimeInterface $date_publication): static
+    {
+        $this->date_publication = $date_publication;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getNombreEtages(): ?int
+    {
+        return $this->nombre_etages;
+    }
+
+    public function setNombreEtages(int $nombre_etages): static
+    {
+        $this->nombre_etages = $nombre_etages;
+
+        return $this;
+    }
+
+    public function getNombreFacades(): ?int
+    {
+        return $this->nombre_facades;
+    }
+
+    public function setNombreFacades(int $nombre_facades): static
+    {
+        $this->nombre_facades = $nombre_facades;
+
+        return $this;
+    }
+
+    public function getParking(): ?int
+    {
+        return $this->parking;
+    }
+
+    public function setParking(int $parking): static
+    {
+        $this->parking = $parking;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Statistique.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatistiqueRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StatistiqueRepository::class)]
+class Statistique
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $nombre_visite = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombreVisite(): ?int
+    {
+        return $this->nombre_visite;
+    }
+
+    public function setNombreVisite(int $nombre_visite): static
+    {
+        $this->nombre_visite = $nombre_visite;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
